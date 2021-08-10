c9b9c8b290fec3ab30db2d865b368e80eb204d2b
lbmenlion <lbmenlion@gmail.com>
2021-08-10 22:13:14 +0800

    commit 21_08_09_P20_Game_Mine_clearance file

--- 21_08_09_P20_Game_Mine_clearance/21_08_09_P20_Game_Mine_clearance/game.h
+++ 21_08_09_P20_Game_Mine_clearance/21_08_09_P20_Game_Mine_clearance/game.h
@@ -1,1 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#define ROW 9
+#define COL 9
+#define ROWS ROW+2
+#define COLS COL+2
+
+//¶¨Òå10¸öÀ×
+#define EASY_COUNT 80
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+void InitBoard(char board[ROWS][COLS], int rows, int cols, char set);
+void DisplayBoard(char board[ROWS][COLS], int row, int col);
+void SetMine(char board[ROWS][COLS],int row, int col);
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
--- 21_08_09_P20_Game_Mine_clearance/21_08_09_P20_Game_Mine_clearance/main.c
+++ 21_08_09_P20_Game_Mine_clearance/21_08_09_P20_Game_Mine_clearance/main.c
@@ -1,5 +1,59 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-void 
+#include "game.h"
+void menu()
+{
+	printf("********************************\n");
+	printf("**********   1.Play   **********\n");
+	printf("**********   0.Exit   **********\n");
+	printf("********************************\n");
+}
+void game()
+{
+	printf("扫雷开始！\n");
+	//雷的信息存储！
+	//1.布置好布置的雷的信息
+	char mine[ROWS][COLS] = { 0 };//11*11
+	//2.排查出的雷的信息
+	char show[ROWS][COLS] = { 0 };//11*11
+	//初识化棋盘信息
+	InitBoard(mine, ROWS, COLS, '0');
+	InitBoard(show, ROWS, COLS, '*');
+	//打印棋盘信息
+	//DisplayBoard(mine, ROW, COL);//雷阵信息是看不见的。
+	DisplayBoard(show, ROW, COL);
+	//布置雷阵（埋雷）
+	SetMine(mine, ROW, COL);
+	DisplayBoard(mine, ROW, COL);//埋完雷的情况
+	//排雷
+	FindMine(mine,show,ROW,COL); 
+}
+void test() 
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+
+	do 
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			printf("开始游戏！\n");
+			game();
+			break;
+		case 0:
+			printf("退出游戏！\n");
+			break;
+		default:
+			printf("选择错误，请重新选择！\n");
+			break;
+		}
+	}
+	while (input);
+}
+
 int main()
 {
 	test();
